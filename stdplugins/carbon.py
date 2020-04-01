bf68126fe4c5ca70f76c92d66cd7228a839ca578
spiderthehacker <48144985+spiderthehacker@users.noreply.github.com>
2020-04-01 09:35:15 +0545

    Update carbon.py

--- stdplugins/carbon.py
+++ stdplugins/carbon.py
@@ -62,7 +62,7 @@ async def carbon_api(e):
    await e.client.send_file(
          e.chat_id,
          file,
-         caption="Carbon by [@r4v4n4](https://www.github.com/ravana69/pornhub)",
+         caption="Carbon by [@spider_encrypted]",
          force_document=False,
          reply_to=e.message.reply_to_msg_id,
          )
