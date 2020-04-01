96d5bf1125946b2239e1cd1629b109e22d117af9
spiderthehacker <48144985+spiderthehacker@users.noreply.github.com>
2020-04-01 09:43:24 +0545

    Update deploy.py

--- stdplugins/deploy.py
+++ stdplugins/deploy.py
@@ -32,16 +32,16 @@ async def _(event):
 
         animation_chars = [
         
-            "**Heroku Connecting To Latest Github Build (Pornhub)**",
-            "**Build started by user** @r4v4n4",
-            "**Deploy** `535a74f0` **by user** @r4v4n4",
+            "**Heroku Connecting To Latest Github Build (Spidey)**",
+            "**Build started by user** @spider_encrypted",
+            "**Deploy** `535a74f0` **by user** @spider_encrypted",
             "**Restarting Heroku Server...**",
             "**State changed from up to starting**",    
             "**Stopping all processes with SIGTERM**",
             "**Process exited with** `status 143`",
             "**Starting process with command** `python3 -m stdborg`",
             "**State changed from starting to up**",
-            "__INFO:UniBorg:Logged in as 557667062__",
+            "__INFO:UniBorg:Logged in as 862040590__",
             "__INFO:UniBorg:Successfully loaded all plugins__",
             "**Build Succeeded**"
 
