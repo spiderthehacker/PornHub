58bb8d5f95d1650f3ff27d3d4e28e2a76f562146
spiderthehacker <48144985+spiderthehacker@users.noreply.github.com>
2020-04-01 12:37:06 +0545

    Update bomber.py

--- stdplugins/bomber.py
+++ stdplugins/bomber.py
@@ -2,7 +2,7 @@
 Format .bombntc [phone number]"""
 import asyncio
 import requests
-from userbot.utils import admin_cmd
+from uniborg.util import admin_cmd
 @borg.on(admin_cmd("ntc (.*)"))
 async def _(event):
     num=0
